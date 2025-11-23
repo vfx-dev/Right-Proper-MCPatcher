5fff852bf0fe071993b1887dc4ff6e78b901eef8
Ven <tudurap.com@gmail.com>
2025-11-23 23:50:11 +0000

    fix: Crash outside of dev

--- src/main/java/com/falsepattern/mcpatcher/internal/mixin/client/cit/RendererLivingEntityMixin.java
+++ src/main/java/com/falsepattern/mcpatcher/internal/mixin/client/cit/RendererLivingEntityMixin.java
@@ -77,10 +77,11 @@ public abstract class RendererLivingEntityMixin implements ICITArmorRenderer {
         }
     }
 
-    @Definition(id = "j",
-                local = @Local(type = int.class,
-                               ordinal = 0))
-    @Expression("(j & 15) == 15")
+//    @Definition(id = "j",
+//                local = @Local(type = int.class,
+//                               ordinal = 0))
+//    @Expression("(j & 15) == 15")
+    @Expression("(? & 15) == 15") // TODO: Doesn't work out of dev if I specify the local, some deobf jank?
     @ModifyExpressionValue(method = "doRender(Lnet/minecraft/entity/EntityLivingBase;DDDFF)V",
                            at = @At("MIXINEXTRAS:EXPRESSION"),
                            require = 1)
